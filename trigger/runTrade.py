f55ff13e387f4df27c08752eacfddb6c6a5fde97
dan-ledezma-twc <dan.ledezma@troyweb.com>
2024-11-11 18:49:59 -0500

    Fixing nested import file structure

--- trigger/runTrade.py
+++ trigger/runTrade.py
@@ -18,13 +18,12 @@ def run_watertrading(in_path_trading, ex_path_trading):
         raise Exception("Error: Target folder is empty or does not exist")
 
     latest_file = max(list_of_files, key=os.path.getmtime)
-    output_dir = os.path.join(in_path_trading, "match-detail")
     file_names = {
-        "requests": "_requests.json",
+        "requests": latest_file,
         "distance": "_distance.json",
         "matches": "_matches.json",
         "profits": "_profits.json",
     }
 
     # Run water trading optimization model
-    WT.run_optimization_models(data_dir=ex_path_trading, output_dir=output_dir, file_names=file_names, Update_distance_matrix=True)
+    WT.run_optimization_models(data_dir=ex_path_trading, output_dir=in_path_trading, file_names=file_names, Update_distance_matrix=True)
